9b184f51e2e476467b5e863e9ad353d28f91d790
Sandro Herrera Pallares <sandrohp88@gmail.com>
2019-07-17 15:55:39 -0700

    Basic paycheck layout

---
+++ client/src/views/paycheckCard/PaycheckCard.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import List from "@material-ui/core/List";
+import ListItemText from "@material-ui/core/ListItemText";
+import PropTypes from "prop-types";
+// Styles
+const useStyles = makeStyles(theme => ({
+  card: {
+    maxWidth: 250,
+    margin: theme.spacing()
+  },
+  header: {
+    textAlign: "center"
+  },
+  listItem: {
+    margin: theme.spacing(0, 2)
+  },
+  cardContent: {
+    marginLeft: "12%"
+  }
+}));
+
+export const PaycheckCard = ({
+  date,
+  expenses = [],
+  totalIncome = 0,
+  debtPaid = [],
+  income = 0
+}) => {
+  const classes = useStyles();
+
+  const totalExpenses = expenses.reduce((accumulator, { value }) => accumulator + value, 0);
+  const totalDebtPaid = debtPaid.reduce((accumulator, { value }) => accumulator + value, 0);
+  return (
+    <Card className={classes.card}>
+      <CardHeader title={`Paycheck ${date}`} className={classes.header} />
+      <CardContent className={classes.cardContent}>
+        <Typography>{`Total income: ${income}`}</Typography>
+        <List>
+          <Typography>{`Total expenses: ${totalExpenses}`}</Typography>
+          {expenses.map((expense, index) => (
+            <ListItemText key={index} className={classes.listItem}>{`${expense.description}: ${
+              expense.value
+            }`}</ListItemText>
+          ))}
+        </List>
+        <List>
+          <Typography>{`Debt Paid: ${totalDebtPaid}`}</Typography>
+          {debtPaid.map((debt, index) => (
+            <ListItemText key={index + debt.description} className={classes.listItem}>{`${
+              debt.description
+            }: ${debt.amount}`}</ListItemText>
+          ))}
+        </List>
+      </CardContent>
+    </Card>
+  );
+};
+PaycheckCard.propTypes = {
+  expenses: PropTypes.arrayOf(
+    PropTypes.shape({
+      description: PropTypes.string.isRequired,
+      value: PropTypes.number.isRequired
+    })
+  ).isRequired,
+  debtPaid: PropTypes.arrayOf(
+    PropTypes.shape({
+      description: PropTypes.string.isRequired,
+      amount: PropTypes.number.isRequired
+    })
+  ).isRequired,
+  date: PropTypes.string.isRequired,
+  income: PropTypes.number.isRequired
+};
+export default PaycheckCard;
