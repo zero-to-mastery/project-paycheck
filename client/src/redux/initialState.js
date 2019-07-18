91ef20086ddd4380f25e8b780e81ea4f0a52d7d1
Sandro Herrera Pallares <sandrohp88@gmail.com>
2019-07-17 21:39:19 -0700

    Add initial store state

---
+++ client/src/redux/initialState.js
@@ -0,0 +1,19 @@
+export default {
+  user: {
+    // token: String,
+    // id: String,
+    // firstName: String,
+    // lastName: String,
+    // email: String,
+    // password: String
+  },
+  expenses: [
+    {
+      // id: String,
+      // description: String,
+      // value: String,
+      // createdAt: Date,
+      // paid: Boolean
+    }
+  ]
+};
