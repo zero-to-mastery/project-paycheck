ed030d9527f6040848bf7110aae8202ab3245485
Sandro Herrera Pallares <sandrohp88@gmail.com>
2019-07-17 23:47:40 -0700

    Update root reducer to include expenses in the global state

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from "redux";
 import userReducer from "./user";
-const rootReducer = combineReducers({ user: userReducer });
+import expensesReducer from "./expenses";
+const rootReducer = combineReducers({ user: userReducer, expenses: expensesReducer });
 
 export default rootReducer;
