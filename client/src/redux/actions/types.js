3e75a632976f4c0fffbf07dbe5a4bf8a78bb7cf4
Sandro Herrera Pallares <sandrohp88@gmail.com>
2019-07-17 21:35:40 -0700

    Add file to store action types constants

---
+++ client/src/redux/actions/types.js
@@ -0,0 +1,5 @@
+// user action types
+export const CURRENT_USER = "CURRENT_USER";
+// expenses actions types
+export const FETCH_EXPENSES = "FETCH_EXPENSES";
+export const FETCH_EXPENSES_SUCCESS = "FETCH_EXPENSES_SUCCESS";
